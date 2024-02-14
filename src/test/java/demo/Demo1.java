d6e931d4053f493befb89c725b936a2734a72f7a
Veenaaaa <kamathveena28@gmail.com>
2024-02-14 16:04:12 +0530

    first commit

---
+++ src/test/java/demo/Demo1.java
@@ -0,0 +1,18 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+	
+	@Test
+	public void hello()
+	{
+		System.out.println("hello");
+	}
+	@Test
+	public void bye()
+	{
+		System.out.println("bye");
+	}
+
+}
